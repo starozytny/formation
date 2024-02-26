5bfc4b7c968a9be3ca477d634d7d68b49bbf5a01
shanbora <chanbora@logilink.fr>
2024-02-26 17:53:19 +0100

    style buttons

--- assets/user/js/components/Elements/Button.jsx
+++ assets/user/js/components/Elements/Button.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export function ButtonACancel ({ link, children = "Annuler" })
 {
 	return <a href={link}
-			  className="inline-block rounded-md font-semibold py-2 px-4 text-center transition-colors hover:bg-gray-100">
+			  className="inline-flex justify-center text-center rounded-md bg-white py-2 px-4 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
 		{children}
 	</a>
 }
 
 export function ButtonSubmit ({ children }) {
 	return <button type="submit"
-				   className="inline-block rounded-md bg-blue-600 font-semibold py-2 px-4 text-center text-slate-50 transition-colors hover:bg-blue-500"
+				   className="inline-flex justify-center text-center rounded-md bg-blue-600 py-2 px-4 text-sm font-semibold text-slate-50 shadow-sm ring-1 ring-inset ring-blue-600 hover:bg-blue-500"
 	>
 		{children}
 	</button>
